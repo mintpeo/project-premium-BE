bb28b5b3ebfeb61b80d8b02eb6ee9bc51382b18e
MiniTi <macbook@MiniTi-Mac.local>
2026-04-20 14:45:29 +0700

    First Commit

---
+++ src/main/java/com/tmdt/projectpremium/ProjectPremiumApplication.java
@@ -0,0 +1,13 @@
+package com.tmdt.projectpremium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectPremiumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectPremiumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tmdt/projectpremium/ProjectPremiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmdt.projectpremium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectPremiumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
